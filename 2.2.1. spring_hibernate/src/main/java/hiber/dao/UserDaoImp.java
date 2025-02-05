02fd8e2578e756350486b7f156c1f38872eb55eb
konstantin <kvbryukhanov@gmail.com>
2025-02-05 18:01:23 +0300

    2025-02-05

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,9 +1,12 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
@@ -11,19 +14,45 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    @Transactional
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    @Transactional(readOnly = true)
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+    @Transactional
+    public void addCar(Car car) {
+        sessionFactory.getCurrentSession().save(car);
+        car.getUser().setCar(car);
+    }
+
+    @Override
+    @Transactional
+    public User getUserByCarModel(String carModel) {
+        Query<User> query = sessionFactory.getCurrentSession().
+                createQuery("from User where car.model = :carModel", User.class).
+                setParameter("carModel", carModel);
+        return query.uniqueResult();
+    }
+
+    @Override
+    @Transactional
+    public User getUserByCarSeries(int carSeries) {
+        Query<User> query = sessionFactory.getCurrentSession().
+                createQuery("from User where car.series = :carSeries", User.class).
+                setParameter("carSeries", carSeries);
+        return query.uniqueResult();
+    }
 }
